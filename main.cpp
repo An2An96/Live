08f0542c37fb43748df1d4b59156063c9704dd8d
An2An <an2an96@bk.ru>
2017-05-01 16:03:25 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <view.h>
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);   //  управляет главным потоком и настройками GUI
+    Window w;   //  создание объекта класса главного окна
+    w.show();   //  отображение созданного окна
+    return app.exec();  //  начало обработки событий
+}
